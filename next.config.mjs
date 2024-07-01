f85b6584763251df2c007ef3f34cf26448539cad
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-07-01 23:35:05 +0900

    Adding contents github

--- next.config.mjs
+++ next.config.mjs
@@ -4,23 +4,12 @@
 
 
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   */
+
   output: "export",
   basePath: '/cv-page',
-  // assetPrefix: prefix,
-  /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   */
+
   reactStrictMode: true,
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   */
+
   images: {
     unoptimized: true,
   },
