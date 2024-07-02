fd13327a9978e6cc562de366acd62599a87e481b
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-07-02 14:06:34 +0900

    404 issue reslove

--- app/pages/[id]/page.tsx
+++ app/pages/[id]/page.tsx
@@ -10,6 +10,8 @@ export async function generateStaticParams() {
     { id: '1', title: 'Project 1', description: 'Detailed description of Project 1.', imageUrl: '/KBH00.github.io/vercel.svg' },
     { id: '2', title: 'Project 2', description: 'Detailed description of Project 2.', imageUrl: '/KBH00.github.io/vercel.svg' },
     { id: '3', title: 'Project 3', description: 'Detailed description of Project 3.', imageUrl: '/KBH00.github.io/vercel.svg' },
+    { id: '4', title: 'Project 4', description: 'Detailed description of Project 4.', imageUrl: '/KBH00.github.io/vercel.svg' },
+
   ];
 
   return projects.map((project) => ({
